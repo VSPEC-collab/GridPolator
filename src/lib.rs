1d30a4eb537722d8e9d29e0b80124f6342006cf3
Ted Johnson <ted.johnson@unlv.edu>
2023-11-09 11:57:13 -0800

    use numpy and ndarray bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
-use pyo3::prelude::*;
-use pyo3::types::{IntoPyArray, PyArray1};
-
+/// The GridPolator binning module in Rust.
+/// Benchmarks have shown that this implementation is
+/// 50 times faster than Python.
+use numpy::ndarray::Array1;
+use numpy::{IntoPyArray, PyArray1, PyReadonlyArray1};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 mod binning;
 
-
-#[pyfunction]
-fn bin_spectra(
-    _py: Python,
-    wl_old: Vec<f64>,
-    flux_old: Vec<f64>,
-    wl_new: Vec<f64>,
-) -> PyResult<Vec<f64>> {
-    Ok(binning::bin_spectra(wl_old, flux_old, &wl_new))   
-}
-
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn _gridpolator(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(bin_spectra, m)?)?;
+fn _gridpolator<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
+    #[pyfn(m)]
+    fn bin_spectra<'py>(
+        _py: Python<'py>,
+        wl_old: PyReadonlyArray1<'py, f64>,
+        flux_old: PyReadonlyArray1<'py, f64>,
+        wl_new: PyReadonlyArray1<'py, f64>,
+    ) -> &'py PyArray1<f64> {
+        let wl_old: Array1<f64> = wl_old.to_owned_array();
+        let flux_old: Array1<f64> = flux_old.to_owned_array();
+        let wl_new: Array1<f64> = wl_new.to_owned_array();
+        let flux_new = binning::bin_spectra(wl_old, flux_old, &wl_new);
+        return flux_new.into_pyarray(_py);
+    }
     Ok(())
 }
